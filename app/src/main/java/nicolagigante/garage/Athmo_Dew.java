b94f0f7f56cc71f3b639cdae578e5ec1d00fae22
nicogig <nicolagigante@outlook.com>
2016-01-23 23:21:59 +0100

    HomeCloud 5. Now keeping track of why you have that headache.
    HomeCloud 5 is here with a whole new icon,wohoo! It also introduces full
    Athmo support. Please keep in mind that in order to work properly you
    must update Athmo's FW to version 3.0+, the first to support
    multilanguage and graphing in the app. Also, we've added credits to the
    lovely folks over at icons8.com who make great icons for free (or dirt
    cheap.)! Thank you guys, you're awesome!

--- app/src/main/java/nicolagigante/garage/Athmo_Dew.java
+++ app/src/main/java/nicolagigante/garage/Athmo_Dew.java
@@ -2,32 +2,124 @@ package nicolagigante.garage;
 
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
 
 /**
  * Created by Nicola on 17/1/2016.
  */
 public class Athmo_Dew extends Fragment {
 
+    public String text;
+    private String[] txtTempString;
+    private int[] intTemp;
+    private String stringTemp;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Defines the xml file for the fragment
-        View view = inflater.inflate(R.layout.fragment_temp, container, false);
-        // Setup handles to view objects here
-        // etFoo = (EditText) view.findViewById(R.id.etFoo);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_dew, container, false);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String ipathmo = prefs.getString("IPAthmo", "");
+        new ParseURL().execute(new String[]{"http://"+ ipathmo +"/point.txt"});
         WebView webView = (WebView)view.findViewById(R.id.webView);
-        webView.loadUrl("http://" + ipathmo + "/dew");
+        webView.loadUrl("http://" + ipathmo + "/" + getString(R.string.dewlink));
         webView.setBackgroundColor(Color.parseColor("#26c6da"));
         return view;
     }
+
+    private void parseData(String string, View view){
+       // string = string.replaceAll("-", "");
+        txtTempString = string.split(";");
+        ArrayList<BarEntry> yVals = new ArrayList<>();
+        ArrayList<String> xVals = new ArrayList<>();
+        for (int i = 0; i < txtTempString.length; i++){
+            yVals.add(new BarEntry(Float.parseFloat(txtTempString[i]), i));
+            xVals.add(String.valueOf(i));
+        }
+        View parent = (View)view.getParent();
+        BarChart chart = (BarChart)parent.findViewById(R.id.chart4);
+        BarDataSet set1 = new BarDataSet(yVals , "");
+        set1.setColor(Color.parseColor("#FFFFFF"));
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+        BarData data = new BarData(xVals, dataSets);
+        Legend legend = chart.getLegend();
+        legend.setEnabled(false);
+        YAxis leftAxis = chart.getAxisLeft();
+        YAxis rightAxis = chart.getAxisRight();
+        XAxis xAxis = chart.getXAxis();
+        leftAxis.setGridColor(Color.parseColor("#26c6da"));
+        leftAxis.setTextColor(Color.parseColor("#00000000"));
+        rightAxis.setTextColor(Color.parseColor("#00000000"));
+        rightAxis.setGridColor(Color.parseColor("#26c6da"));
+        rightAxis.setAxisLineColor(Color.parseColor("#26c6da"));
+        xAxis.setTextColor(Color.parseColor("#00000000"));
+        xAxis.setAxisLineColor(Color.parseColor("#26c6da"));
+        xAxis.setGridColor(Color.parseColor("#26c6da"));
+        chart.setData(data);
+        chart.setBackgroundColor(Color.parseColor("#26c6da"));
+        chart.setDescription("");
+        chart.setDrawGridBackground(false);
+        chart.setDrawBorders(false);
+        set1.setValueTextColor(Color.parseColor("#FFFFFF"));
+        set1.setValueTextSize(10);
+        chart.animateY(1000, Easing.EasingOption.Linear);
+    }
+
+    private class ParseURL extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute()
+        {
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+            StringBuffer buffer = new StringBuffer();
+            try {
+                Log.d("JSwa", "Connecting to [" + strings[0] + "]");
+                Document doc = Jsoup.connect(strings[0]).get();
+                Log.d("JSwa", "Connected to [" + strings[0] + "]");
+                text = doc.body().text(); // "An example link"
+                buffer.append(text);
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
+            return text;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            super.onPostExecute(result);
+            parseData(result, getView());
+        }
+
+    }
+
 
 }
--- app/src/main/java/nicolagigante/garage/Athmo_Temp.java
+++ app/src/main/java/nicolagigante/garage/Athmo_Temp.java
@@ -1,11 +1,7 @@
 package nicolagigante.garage;
 
-import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Color;
-import android.graphics.DashPathEffect;
-import android.graphics.PointF;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -15,100 +11,83 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.androidplot.util.PixelUtils;
-import com.androidplot.xy.CatmullRomInterpolator;
-import com.androidplot.xy.LineAndPointFormatter;
-import com.androidplot.xy.PointLabelFormatter;
-import com.androidplot.xy.SimpleXYSeries;
-import com.androidplot.xy.XYPlot;
-import com.androidplot.xy.XYSeries;
-
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.util.Arrays;
-import java.util.concurrent.ExecutionException;
+import java.util.ArrayList;
 
 /**
  * Created by Nicola on 17/1/2016.
  */
 public class Athmo_Temp extends Fragment {
 
-    private XYPlot plot;
-    private TextView txtview;
-    private String txtString;
+    public String text;
+    private String[] txtTempString;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Defines the xml file for the fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_temp, container, false);
-        // Setup handles to view objects here
-        // etFoo = (EditText) view.findViewById(R.id.etFoo);
-
-// initialize our XYPlot reference:
-        plot = (XYPlot) view.findViewById(R.id.mySimpleXYPlot);
-        txtview = (TextView)view.findViewById(R.id.textView11);
-
-        // Create a couple arrays of y-values to plot:
-        Number[] series1Numbers = {1, 8, 5, 2, 7, 4};
-
-        // Turn the above arrays into XYSeries':
-        XYSeries series1 = new SimpleXYSeries(
-                Arrays.asList(series1Numbers),          // SimpleXYSeries takes a List so turn our array into a List
-                SimpleXYSeries.ArrayFormat.Y_VALS_ONLY, // Y_VALS_ONLY means use the element index as the x value
-                "Series1");                             // Set the display title of the series
-
-        // same as above
-
-
-        // Create a getFormatter to use for drawing a series using LineAndPointRenderer
-        // and configure it from xml:
-        LineAndPointFormatter series1Format = new LineAndPointFormatter();
-        series1Format.setPointLabelFormatter(new PointLabelFormatter());
-        series1Format.configure(getActivity(),
-                R.xml.line_point_formatter_with_labels);
-
-        // add a new series' to the xyplot:
-        plot.addSeries(series1, series1Format);
-
-        // reduce the number of range labels
-        plot.setTicksPerRangeLabel(3);
-        plot.getGraphWidget().setDomainLabelOrientation(-45);
-        try {
-            txtString = new ParseURL()
-                                      .execute(new String[]{"http://192.168.0.120/values.txt"})
-                                      .get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        txtview.setText(txtString);
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String ipathmo = prefs.getString("IPAthmo", "");
+        new ParseURL().execute(new String[]{"http://"+ ipathmo +"/temp.txt"});
         WebView webView = (WebView)view.findViewById(R.id.webView);
-        webView.loadUrl("http://" + ipathmo + "/temperature");
+        webView.loadUrl("http://" + ipathmo + "/" + getString(R.string.templink));
         webView.setBackgroundColor(Color.parseColor("#26c6da"));
         return view;
     }
 
-    private class ParseURL extends AsyncTask<String, Void, String> {
+    private void parseData(String string, View view){
+        txtTempString = string.split(";");
+        ArrayList<BarEntry> yVals = new ArrayList<>();
+        ArrayList<String> xVals = new ArrayList<>();
+        for (int i = 0; i < txtTempString.length; i++){
+            yVals.add(new BarEntry(Float.parseFloat(txtTempString[i]), i));
+            xVals.add(String.valueOf(i));
+        }
+        View parent = (View)view.getParent();
+        BarChart chart = (BarChart)parent.findViewById(R.id.chart);
+        BarDataSet set1 = new BarDataSet(yVals , "");
+        set1.setColor(Color.parseColor("#FFFFFF"));
+        ArrayList<IBarDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+        BarData data = new BarData(xVals, dataSets);
+        Legend legend = chart.getLegend();
+        legend.setEnabled(false);
+        YAxis leftAxis = chart.getAxisLeft();
+        YAxis rightAxis = chart.getAxisRight();
+        XAxis xAxis = chart.getXAxis();
+        leftAxis.setGridColor(Color.parseColor("#26c6da"));
+        leftAxis.setTextColor(Color.parseColor("#00000000"));
+        rightAxis.setTextColor(Color.parseColor("#00000000"));
+        rightAxis.setGridColor(Color.parseColor("#26c6da"));
+        rightAxis.setAxisLineColor(Color.parseColor("#26c6da"));
+        xAxis.setTextColor(Color.parseColor("#00000000"));
+        xAxis.setAxisLineColor(Color.parseColor("#26c6da"));
+        xAxis.setGridColor(Color.parseColor("#26c6da"));
+        chart.setData(data);
+        chart.setBackgroundColor(Color.parseColor("#26c6da"));
+        chart.setDescription("");
+        chart.setDrawGridBackground(false);
+        chart.setDrawBorders(false);
+        set1.setValueTextColor(Color.parseColor("#FFFFFF"));
+        set1.setValueTextSize(10);
+        chart.animateY(1000, Easing.EasingOption.Linear);
+    }
 
-        ProgressDialog progressDialog;
+    private class ParseURL extends AsyncTask<String, Void, String> {
 
         @Override
         protected void onPreExecute()
         {
-            progressDialog = ProgressDialog.show(getActivity(), "Progress Dialog Title Text","Process Description Text", true);
-
-            //do initialization of required objects objects here
-        };
+        }
 
         @Override
         protected String doInBackground(String... strings) {
@@ -118,36 +97,12 @@ public class Athmo_Temp extends Fragment {
                 Log.d("JSwa", "Connecting to [" + strings[0] + "]");
                 Document doc = Jsoup.connect(strings[0]).get();
                 Log.d("JSwa", "Connected to [" + strings[0] + "]");
-                // Get document (HTML page) title
-                String title = doc.title();
-                Log.d("JSwA", "Title [" + title + "]");
-                buffer.append("Title: " + title + "\r\n");
-
-                // Get meta info
-                Elements metaElems = doc.select("meta");
-                buffer.append("META DATA\r\n");
-                for (Element metaElem : metaElems) {
-                    String name = metaElem.attr("name");
-                    String content = metaElem.attr("content");
-                    buffer.append("name [" + name + "] - content [" + content + "] \r\n");
-                }
-
-                Elements topicList = doc.select("h2.topic");
-                buffer.append("Topic list\r\n");
-                for (Element topic : topicList) {
-                    String data = topic.text();
-
-                    buffer.append("Data [" + data + "] \r\n");
-                }
-
-
+                text = doc.body().text(); // "An example link"
+                buffer.append(text);
             } catch (Throwable t) {
                 t.printStackTrace();
             }
-
-
-
-            return buffer.toString();
+            return text;
         }
 
 
@@ -155,8 +110,8 @@ public class Athmo_Temp extends Fragment {
         protected void onPostExecute(String result)
         {
             super.onPostExecute(result);
-            progressDialog.dismiss();
-        };
+            parseData(result, getView());
+        }
 
     }
 

7cf174eb4a996d0c003919fa526eb3819f3c4978
Nicola <nicolagigante@outlook.com>
2016-08-18 17:17:39 +0100

    HomeCloud 7.0.6
    Minor fixes and improvements

--- app/src/main/java/nicolagigante/garage/Contextual_Notifications/AddGeofenceFragment.java
+++ app/src/main/java/nicolagigante/garage/Contextual_Notifications/AddGeofenceFragment.java
@@ -1,68 +1,102 @@
 package nicolagigante.garage.Contextual_Notifications;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 import nicolagigante.garage.R;
 
 public class AddGeofenceFragment extends DialogFragment {
 
-  // region Properties
+    // region Properties
 
-  private ViewHolder viewHolder;
+    private ViewHolder viewHolder;
+    private String mLatitudeText;
+    private String mLongitudeText;
 
-  private ViewHolder getViewHolder() {
-    return viewHolder;
-  }
+    private ViewHolder getViewHolder() {
+        return viewHolder;
+    }
 
-  AddGeofenceFragmentListener listener;
-  public void setListener(AddGeofenceFragmentListener listener) {
-    this.listener = listener;
-  }
+    final String gpsLocationProvider = LocationManager.GPS_PROVIDER;
+    final String networkLocationProvider = LocationManager.NETWORK_PROVIDER;
 
-  // endregion
+    AddGeofenceFragmentListener listener;
 
-  // region Overrides
+    public void setListener(AddGeofenceFragmentListener listener) {
+        this.listener = listener;
+    }
 
-  @Override
-  public Dialog onCreateDialog(Bundle savedInstanceState) {
-    LayoutInflater inflater = getActivity().getLayoutInflater();
-    View view = inflater.inflate(R.layout.dialog_add_geofence, null);
+    // endregion
 
-    viewHolder = new ViewHolder();
-    viewHolder.populate(view);
+    // region Overrides
 
-    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-    builder.setView(view)
-            .setPositiveButton(R.string.Add, null)
-            .setNegativeButton(R.string.Cancel, new DialogInterface.OnClickListener() {
-              public void onClick(DialogInterface dialog, int id) {
-                AddGeofenceFragment.this.getDialog().cancel();
-                if (listener != null) {
-                  listener.onDialogNegativeClick(AddGeofenceFragment.this);
-                }
-              }
-            });
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        LocationManager locationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Intent i4extra = new Intent(getContext(), PermissionDenied.class);
+            startActivity(i4extra);
+        }
+        Location lastKnownLocation_byGps = locationManager.getLastKnownLocation(gpsLocationProvider);
+        if (lastKnownLocation_byGps != null) {
+            mLatitudeText = String.valueOf(lastKnownLocation_byGps.getLatitude());
+            mLongitudeText = String.valueOf(lastKnownLocation_byGps.getLongitude());
+            Log.d("Latitude", mLatitudeText);
+            Log.d("Longitude", mLongitudeText);
+        }
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_add_geofence, null);
 
-    final AlertDialog dialog = builder.create();
+        viewHolder = new ViewHolder();
+        viewHolder.populate(view);
 
-    dialog.setOnShowListener(new DialogInterface.OnShowListener() {
-      @Override
-      public void onShow(DialogInterface dialogInterface) {
-        Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
-        positiveButton.setOnClickListener(new View.OnClickListener() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(view)
+                .setPositiveButton(R.string.Add, null)
+                .setNegativeButton(R.string.Cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        AddGeofenceFragment.this.getDialog().cancel();
+                        if (listener != null) {
+                            listener.onDialogNegativeClick(AddGeofenceFragment.this);
+                        }
+                    }
+                });
 
-          @Override
-          public void onClick(View view) {
+        final AlertDialog dialog = builder.create();
+
+        dialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
+                Button positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE);
+                positiveButton.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+
+
               // 1. Check for valid data
               if (dataIsValid()) {
                   // 2. Create a named geofence
@@ -84,50 +118,50 @@ public class AddGeofenceFragment extends DialogFragment {
                   // 4. Display an error message
                   showValidationErrorToast();
               }
-          }
+                    }
 
+                });
+
+            }
         });
 
-      }
-    });
-
-    return dialog;
-  }
-
-  // endregion
-
-  // region Private
-
-  private boolean dataIsValid() {
-    boolean validData = true;
-
-    String name = getViewHolder().nameEditText.getText().toString();
-    String latitudeString = getViewHolder().latitudeEditText.getText().toString();
-    String longitudeString = getViewHolder().longitudeEditText.getText().toString();
-    String radiusString = getViewHolder().radiusEditText.getText().toString();
-
-    if (TextUtils.isEmpty(name) || TextUtils.isEmpty(latitudeString)
-            || TextUtils.isEmpty(longitudeString) || TextUtils.isEmpty(radiusString)) {
-      validData = false;
-    } else {
-      double latitude = Double.parseDouble(latitudeString);
-      double longitude = Double.parseDouble(longitudeString);
-      float radius = Float.parseFloat(radiusString);
-      if ((latitude < Constants.Geometry.MinLatitude || latitude > Constants.Geometry.MaxLatitude)
-              || (longitude < Constants.Geometry.MinLongitude || longitude > Constants.Geometry.MaxLongitude)
-              || (radius < Constants.Geometry.MinRadius || radius > Constants.Geometry.MaxRadius)) {
-        validData = false;
-      }
+        return dialog;
     }
 
-    return validData;
-  }
+    // endregion
 
-  private void showValidationErrorToast() {
-    Toast.makeText(getActivity(), getActivity().getString(R.string.Toast_Validation), Toast.LENGTH_SHORT).show();
-  }
+    // region Private
 
-  // endregion
+    private boolean dataIsValid() {
+        boolean validData = true;
+
+        String name = getViewHolder().nameEditText.getText().toString();
+        String latitudeString = getViewHolder().latitudeEditText.getText().toString();
+        String longitudeString = getViewHolder().longitudeEditText.getText().toString();
+        String radiusString = getViewHolder().radiusEditText.getText().toString();
+
+        if (TextUtils.isEmpty(name) || TextUtils.isEmpty(latitudeString)
+                || TextUtils.isEmpty(longitudeString) || TextUtils.isEmpty(radiusString)) {
+            validData = false;
+        } else {
+            double latitude = Double.parseDouble(latitudeString);
+            double longitude = Double.parseDouble(longitudeString);
+            float radius = Float.parseFloat(radiusString);
+            if ((latitude < Constants.Geometry.MinLatitude || latitude > Constants.Geometry.MaxLatitude)
+                    || (longitude < Constants.Geometry.MinLongitude || longitude > Constants.Geometry.MaxLongitude)
+                    || (radius < Constants.Geometry.MinRadius || radius > Constants.Geometry.MaxRadius)) {
+                validData = false;
+            }
+        }
+
+        return validData;
+    }
+
+    private void showValidationErrorToast() {
+        Toast.makeText(getActivity(), getActivity().getString(R.string.Toast_Validation), Toast.LENGTH_SHORT).show();
+    }
+
+    // endregion
 
   // region Interfaces
 
@@ -140,7 +174,7 @@ public class AddGeofenceFragment extends DialogFragment {
 
   // region Inner classes
 
-  static class ViewHolder {
+   class ViewHolder {
     EditText nameEditText;
     EditText latitudeEditText;
     EditText longitudeEditText;
@@ -155,6 +189,8 @@ public class AddGeofenceFragment extends DialogFragment {
       latitudeEditText.setHint(String.format(v.getResources().getString(R.string.Hint_Latitude), Constants.Geometry.MinLatitude, Constants.Geometry.MaxLatitude));
       longitudeEditText.setHint(String.format(v.getResources().getString(R.string.Hint_Longitude), Constants.Geometry.MinLongitude, Constants.Geometry.MaxLongitude));
       radiusEditText.setHint(String.format(v.getResources().getString(R.string.Hint_Radius), Constants.Geometry.MinRadius, Constants.Geometry.MaxRadius));
+      latitudeEditText.setText(mLatitudeText);
+      longitudeEditText.setText(mLongitudeText);
     }
   }
 
--- app/src/main/java/nicolagigante/garage/NotSafeInstall.java
+++ app/src/main/java/nicolagigante/garage/NotSafeInstall.java
@@ -1,39 +1,25 @@
 package nicolagigante.garage;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
 
 import nicolagigante.garage.FirstTime.Intro;
 
-/**
- * Created by Nicola on 01/06/2015.
- */
-public class Redirect extends Activity {
-
+public class NotSafeInstall extends AppCompatActivity {
     public static final String FIRST_RUN = "FirstRun";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_not_safe_install);
+    }
+
+    public void goToDone(View view){
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        PackageManager pm = getApplicationContext().getPackageManager();
-        String installerPackageName = pm.getInstallerPackageName(getApplicationContext().getPackageName());
-/*        if (installerPackageName.isEmpty()){
-            Log.d("HomeCloud", "Installed via ADB, skipping.");
-        }else {
-           // Log.d("Whereto", installerPackageName);
-        }*/
-        if ("com.android.vending".equals(installerPackageName)){
-            return;
-        } else {
-            Intent i = new Intent(this, NotSafeInstall.class);
-            startActivity(i);
-        }
         if (prefs.getBoolean(FIRST_RUN, true)) {
             Intent i= new Intent(this, Intro.class);
             startActivity(i);
@@ -51,6 +37,4 @@ public class Redirect extends Activity {
             finish();
         }
     }
-
 }
-
